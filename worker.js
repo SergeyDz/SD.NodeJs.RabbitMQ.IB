f5058726d3b3e33d972e4a151eec3c8139e8a3ce
SergeyDzIntapp <sergey.dziuban@intapp.com>
2016-05-30 14:21:49 +0300

    Correct timeout processing added

--- worker.js
+++ worker.js
@@ -8,47 +8,55 @@ var mq = 'amqp://guest:guest@10.1.1.231:5672';
 var server = http.createServer(function (req, res) {
     var query = url.parse(req.url, true).query;
     log.info('Request received. Client=' + (query ? query.client : 'none'));
-     
-    res.writeHead(200, {'Content-Type': 'application/json'});
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
     res.socket.setTimeout(60 * 1000); // 2 minute timeout
-     
-     function consumer(conn) {
-     var ok = conn.createChannel(on_open);
+    
+    var connection; 
+
+    function consumer(conn) {
+        var ok = conn.createChannel(on_open);
         function on_open(err, ch) {
             if (err != null) {
                 log.error(err);
                 res.end(JSON.stringify(err));
             }
-                
+
             res.socket.once('timeout', function () {
-                     log.warn('sockert timeout');
-                     ch.close();
-                     conn.close();
-                     res.end("{ 'Status' : 'Timeout'}");
-                 });
-                            
+                log.warn('sockert timeout');
+                connection.close();
+                res.end("{ 'Status' : 'Timeout'}");
+            });
+
             ch.assertQueue(q);
             ch.consume(q, function (msg) {
                 if (msg !== null) {
                     var entity = JSON.parse(msg.content.toString());
                     log.info(entity);
                     ch.ack(msg);
-                    res.end(JSON.stringify(entity));                    
-                    return;
+                    ch.close();
+                    return res.end(JSON.stringify(entity));
                 }
-            });
+            });         
         }
-     }
-     
-    require('amqplib/callback_api').connect(mq, function (err, conn) {
+    }
+
+    require('amqplib/callback_api').connect(mq + '?heartbeat=60', function (err, conn) {
         if (err != null) {
             log.error(err);
             res.end(JSON.stringify(err));
         }
         log.info('Consumer started');
+
+        conn.on("close", function () {
+            console.warn("[AMQP] closed");
+        });
+        
+        connection = conn;
+
         consumer(conn);
     });
-    
+
 }).listen(9000, '0.0.0.0');
 
 //server.timeout = 5000;
